5c19f82dcb4e440915916360accf98321ab78ce9
Brian Sanders <bdougsand+cfb@gmail.com>
2019-03-12 21:52:41 -0400

    Placeholder components

---
+++ components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+    Image,
+    Platform,
+    ScrollView,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View,
+} from 'react-native';
+
+const Header = (props) => (
+    <View style={}>
+        <Text>{ props.text }</Text>
+    </View>
+);
+
+export default Header;
