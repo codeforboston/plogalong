17a0dd5c7d464a75cc8c3186458067fdb0633257
Brian Sanders <bdougsand+cfb@gmail.com>
2019-03-12 21:55:46 -0400

    Added a styled header to the Plog page

--- components/Header.js
+++ components/Header.js
@@ -10,9 +10,27 @@ import {
 } from 'react-native';
 
 const Header = (props) => (
-    <View style={}>
-        <Text>{ props.text }</Text>
+    <View style={styles.container}>
+        <Image source={props.icon}  />
+        <Text style={styles.headerText}>{ props.text }</Text>
     </View>
 );
+
+
+const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+      flexDirection: 'row',
+      justifyContent: 'center'
+  },
+
+    headerText: {
+        alignSelf: 'center',
+        color: 'purple',
+        marginLeft: 7,
+        fontSize: 16
+    },
+});
+
 
 export default Header;
--- screens/PlogScreen.js
+++ screens/PlogScreen.js
@@ -10,47 +10,47 @@ import {
 } from 'react-native';
 import { WebBrowser } from 'expo';
 
+import Banner from '../components/Banner';
+import Header from '../components/Header';
+import Map from '../components/Map';
+import SegmentedControl from '../components/SegmentedControl';
 import { MonoText } from '../components/StyledText';
 
 export default class PlogScreen extends React.Component {
   static navigationOptions = {
-    header: null,
+      headerStyle: {
+          backgroundColor: '#fff',
+          borderBottomColor: 'purple',
+          borderBottomWidth: 4
+      },
+      headerTitle: (<Header text="Plog" icon={require('../assets/images/plog.png')}/>)
   };
+
+    static selectedModes = ['Log', 'Flag'];
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedMode: 0
+        };
+    }
+
+    changeMode(idx) {
+        this.setState({ selectedMode: idx });
+    }
 
   render() {
     return (
       <View style={styles.container}>
         <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-          <View style={styles.welcomeContainer}>
-            <Image
-              source={
-                __DEV__
-                  ? require('../assets/images/plog.png')
-                  : require('../assets/images/plog.png')
-              }
-              style={styles.welcomeImage}
+             <Banner></Banner>
+
+            <SegmentedControl selectedIndex={0}
+                              values={PlogScreen.selectedModes}
+                              onChange={(e) => this.changeMode(e.index) }
             />
-          </View>
 
-          <View style={styles.getStartedContainer}>
-            {this._maybeRenderDevelopmentModeWarning()}
-
-            <Text style={styles.getStartedText}>PLOG</Text>
-
-            <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-              <MonoText style={styles.codeHighlightText}>screens/PlogScreen.js</MonoText>
-            </View>
-
-            <Text style={styles.getStartedText}>
-              Change this text and your app will automatically reload.
-            </Text>
-          </View>
-
-          <View style={styles.helpContainer}>
-            <TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
-              <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
-            </TouchableOpacity>
-          </View>
+            <Map/>
         </ScrollView>
 
         <View style={styles.tabBarInfoContainer}>
@@ -63,39 +63,6 @@ export default class PlogScreen extends React.Component {
       </View>
     );
   }
-
-  _maybeRenderDevelopmentModeWarning() {
-    if (__DEV__) {
-      const learnMoreButton = (
-        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-          Learn more
-        </Text>
-      );
-
-      return (
-        <Text style={styles.developmentModeText}>
-          Development mode is enabled, your app will be slower but you can use useful development
-          tools. {learnMoreButton}
-        </Text>
-      );
-    } else {
-      return (
-        <Text style={styles.developmentModeText}>
-          You are not in development mode, your app will run at full speed.
-        </Text>
-      );
-    }
-  }
-
-  _handleLearnMorePress = () => {
-    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
-  };
-
-  _handleHelpPress = () => {
-    WebBrowser.openBrowserAsync(
-      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
-    );
-  };
 }
 
 const styles = StyleSheet.create({
@@ -103,86 +70,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
   contentContainer: {
     paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
   },
 });
