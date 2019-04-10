5b07ca9730cb35dd78006cea7a3f419bbe6b25c7
Brian Sanders <bdougsand+cfb@gmail.com>
2019-04-09 23:19:48 -0400

    Mob programming 4/9: Improved the appearance of the History screen

--- screens/HistoryScreen.js
+++ screens/HistoryScreen.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import {
     FlatList,
+    Image,
     ScrollView,
     StyleSheet,
     Text,
+    View
 } from 'react-native';
+
+import moment from 'moment';
 
 import {connect} from 'react-redux';
 
+import ProfileImage from '../assets/images/profile.png';
+
+
 const Plog = ({plogInfo}) => (
-    <Text>
-        You picked up {plogInfo.get('trashTypes').join(', ')} at {plogInfo.get('when').toString()}
-    </Text>
+    <View style={styles.plogStyle}>
+        <Image source={ProfileImage} style={styles.profileImage} />
+        <View>
+            <Text styles={styles.actionText}>
+                You {plogInfo.get('pickedUp') ? 'picked up' : 'flagged'}&nbsp;
+                {plogInfo.get('trashTypes').join(', ')} at {JSON.stringify(plogInfo.get('location'))}
+            </Text>
+            <Text styles={styles.timeText}>
+                {moment(plogInfo.get('when')).fromNow()}
+            </Text>
+        </View>
+    </View>
+);
+
+const Divider = () => (
+    <View style={styles.divider}></View>
 );
 
 class HistoryScreen extends React.Component {
@@ -20,7 +40,8 @@ class HistoryScreen extends React.Component {
         <ScrollView style={styles.container}>
             <FlatList data={this.props.history.toArray()}
                       renderItem={({item}) => (<Plog plogInfo={item} />)}
-                      keyExtractor={(_, i) => ''+i}>
+                      keyExtractor={(_, i) => ''+i}
+                      ItemSeparatorComponent={Divider}>
             </FlatList>
         </ScrollView>
     );
@@ -30,10 +51,28 @@ class HistoryScreen extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1
+    },
+    plogStyle: {
+        alignItems: 'center',
+        flexDirection: 'row',
+        padding: 10
+    },
+    divider: {
+        borderBottomWidth: 1,
+        borderBottomColor: 'gray'
+    },
+    profileImage: {
+        margin: 10
+    },
+    actionText: {
+
+    },
+    timeText: {
+
     }
 });
 
 
 export default connect(store => ({
-    history: store.log.get('history')
+    history: store.log.get('history').sort((a, b) => (b.get('when') - a.get('when')))
 }))(HistoryScreen);
