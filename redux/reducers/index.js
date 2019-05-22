ebcc699e59cd1a5a5c366958235808910fa4b5f5
psimhadri <psimhadri@vmware.com>
2019-05-22 10:55:18 -0400

    Add preferences to Redux and share activity preference to profile screen

--- redux/reducers/index.js
+++ redux/reducers/index.js
@@ -1,10 +1,10 @@
 import { combineReducers } from "redux";
-
 import log from './plogs';
-
 import users from './users';
+import preferences from './preferences';
 
 export default combineReducers({
   log,
   users,
+  preferences,
 });
