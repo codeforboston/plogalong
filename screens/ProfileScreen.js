b8229069168e284fa35b1e9d1b7a6d8e6997bfd3
Brian Sanders <bdougsand+cfb@gmail.com>
2019-12-10 21:56:13 -0500

    Mob programming 12/10: added fields to Profile screen

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -4,20 +4,33 @@ import {
   ScrollView,
   StyleSheet,
   Text,
+  TextInput,
   View,
 } from 'react-native';
 import { connect } from 'react-redux';
-
 
 import {
   loginWithFacebook,
   logOut,
 } from '../firebase/auth';
+import Banner from '../components/Banner';
 import Button from '../components/Button';
 import { Switch } from 'react-native';
 import { setPreferences, logout} from '../redux/actions';
 
+import Colors from '../constants/Colors';
+import $S from '../styles';
+
 class ProfileScreen extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            params: {
+                homeBase: 'Boston, MA',
+                username: 'Beach Bum'
+            }
+        };
+    }
 
   handleShareActivityPrefChange = (shareActivity) => {
     this.props.updatePreferences({ shareActivity })
@@ -28,37 +41,84 @@ class ProfileScreen extends React.Component {
     }
 
   render() {
+      const {params} = this.state;
     const currentUser = this.props.currentUser && this.props.currentUser.toJS();
+
+      const setParam = param => (text => this.setState(({params}) => ({params: { ...params, [param]: text }})));
+      const created = new Date(parseInt(currentUser.createdAt));
+
+      const createdFormatted = new Intl.DateTimeFormat('en-us', {month: 'long', year: 'numeric'}).format(created);
 
     return (
       <View style={styles.container}>
         <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
 
-          {/* privacy */}
-          <View style={styles.preferencesContainer}>
-            <Text>Share the activity in local feed</Text>
-            <Switch value={this.props.preferences.get("shareActivity")} 
-              onValueChange={this.handleShareActivityPrefChange}></Switch>
-          </View>
-
+          <Banner>
+            Plogging since {createdFormatted}
+          </Banner>
           {/* // alerts */}
 
-          <View style={styles.welcomeContainer}>
+          <View style={styles.personalInfoContainer}>
             <Image
               source={
                 __DEV__
                   ? require('../assets/images/profile.png')
                   : require('../assets/images/profile.png')
               }
-              style={styles.welcomeImage}
+              style={styles.profileImage}
             />
+            <View style={styles.personalInfo}>
+              <Text style={styles.badgeSummary}>
+                0 badges
+              </Text>
+              <Text style={{ fontWeight: 500 }}>
+                Personal Account
+              </Text>
+              <Text style={{ fontWeight: 500 }}>
+                { currentUser.email }
+              </Text>
+            </View>
           </View>
 
           <View>
-            <Text style={styles.welcomeText}>Hello, {currentUser.displayName||'Fellow Plogger'}!</Text>
-            <Button primary onPress={this.goToSignup} title={currentUser.isAnonymous ? 'Create Plogalong Account' : "Link Account" }/>
+            <Text style={styles.welcomeText}>
+              Hello, {currentUser.displayName||'Fellow Plogger'}!
+            </Text>
           </View>
 
+          <View style={$S.inputGroup}>
+            <Text style={$S.inputLabel}>Username (visible to others)</Text>
+            <TextInput style={$S.textInput}
+                       autoCapitalize="none"
+                       value={params.username}
+                       autoCompleteType="username"
+                       onChangeText={setParam('username')}
+            />
+          </View>
+
+          <View style={$S.inputGroup}>
+            <Text style={$S.inputLabel}>Home Base</Text>
+            <TextInput style={$S.textInput}
+                       autoCapitalize="none"
+                       value={params.homeBase}
+                       onChangeText={setParam('homeBase')}
+            />
+          </View>
+
+          <View style={$S.switchInputGroup}>
+            <Text style={$S.inputLabel}>
+              Share activity in Local feed
+            </Text>
+            <Switch value={params.shareActivity}/>
+          </View>
+          <View style={$S.switchInputGroup}>
+            <Text style={$S.inputLabel}>
+              Get email updates ({'< 1/month'})
+            </Text>
+            <Switch value={params.emailUpdatesEnabled} style={{ transform: [{scale: 0.8}] }}/>
+          </View>
+
+          <Button primary onPress={this.goToSignup} title={currentUser.isAnonymous ? 'Create Plogalong Account' : "Link Account" }/>
           <Button primary onPress={logOut} title={currentUser.isAnonymous ? 'Log in as Existing User' : 'Log Out'} />
         </ScrollView>
       </View>
@@ -75,18 +135,30 @@ const styles = StyleSheet.create({
     paddingTop: 30,
     padding: 20
   },
-  welcomeContainer: {
-    alignItems: 'center',
+  personalInfoContainer: {
+      flexDirection: 'row',
     marginTop: 10,
     marginBottom: 20,
   },
-  welcomeImage: {
+  profileImage: {
     width: 100,
     height: 80,
     resizeMode: 'contain',
     marginTop: 3,
     marginLeft: -10,
   },
+    badgeSummary: {
+        borderColor: Colors.borderColor,
+        borderRadius: 5,
+        borderWidth: 1,
+        backgroundColor: '#E5E9F2',
+        marginBottom: 10,
+        padding: 10,
+    },
+    personalInfo: {
+        flexDirection: 'column',
+        flexGrow: 1,
+    },
     welcomeText: {
         textAlign: 'center'
     }
