8699014894041ac1a158bd59bb15bc464c2ad3bd
Brian Sanders <bdougsand+cfb@gmail.com>
2020-04-16 22:42:48 -0400

    Minor changes to AchievementBadge

--- components/AchievementBadge.js
+++ components/AchievementBadge.js
@@ -1,11 +1,14 @@
 import * as React from 'react';
 
 import Colors from '../constants/Colors';
+import AchievementTypes from '../constants/AchievedMockup';
 
 import {
-    StyleSheet,
-    Text,
-    View,
+  StyleSheet,
+  Text,
+  View,
+  ViewStyle,
+  StyleProp
 } from 'react-native';
 
 const styles = StyleSheet.create({
@@ -17,7 +20,8 @@ const styles = StyleSheet.create({
         borderColor: Colors.borderColor,
         padding: 5,
         justifyContent: 'center',
-        alignItems: 'center'
+      alignItems: 'center',
+      marginRight: 10
     },
   completedBadge: {
     borderColor: Colors.selectionColor,
@@ -27,6 +31,10 @@ const styles = StyleSheet.create({
   },
   inProgress: {
     color: '#666666',
+  },
+  iconContainer: {
+    width: 48,
+    height: 48,
   },
     textLarger: {
         fontSize: 18,
@@ -40,28 +48,39 @@ const styles = StyleSheet.create({
     }
 });
 
-function AchievementBadge({badgeImage, completed, detailText, points, progressPercent, textValue}) {
-  const detail = completed ? `+ ${points} points` : detailText;
+/**
+ * @typedef {object} AchievementBadgeProps
+ * @property {React.Component} badgeImage
+ * @property {keyof typeof AchievementTypes} achievement
+ * @property {Date} completed
+ * @property {string} detailText
+ * @property {number} points
+ * @property {number} progressPercent
+ * @property {string} textValue
+ * @property {StyleProp<ViewStyle>} style
+ */
 
-    return (
-      <View style={[styles.achieveBadge, completed && styles.completedBadge]}>
-            <View style=
-                {{
-                    width: 48,
-                    height: 48,
-                    color: '#666666'
-                }}
-            >
-              {React.createElement(badgeImage, { fill: completed ? Colors.selectionColor : '#666666' })}
-            </View>
-        <Text style={[styles.textLarger, completed ?
-                      { color: Colors.selectionColor} : styles.inProgress]}>{textValue}</Text>
-          {
-            detail && <Text style={[styles.textSmaller,
-                                    completed ? { color: Colors.selectionColorLight } : styles.inProgress]}>{detail}</Text>
-          }
-        </View>
-    );
+/** @type {React.FunctionComponent<AchievementBadgeProps>} */
+const AchievementBadge = props => {
+  const {achievement, completed = null, detailText, points, progressPercent, style} = props;
+  const badge = achievement && AchievementTypes[achievement];
+  const detail = completed ? `+ ${points} points` : detailText;
+  const badgeImage = props.badgeImage || (badge && badge.icon);
+  const textValue = props.textValue || (badge && badge.badgeTheme);
+
+  return (
+    <View style={[styles.achieveBadge, completed && styles.completedBadge, style]}>
+      <View style={styles.iconContainer}>
+        {React.createElement(badgeImage, { fill: completed ? Colors.selectionColor : '#666666' })}
+      </View>
+      <Text style={[styles.textLarger, completed ?
+                    { color: Colors.selectionColor} : styles.inProgress]}>{textValue}</Text>
+      {
+        detail && <Text style={[styles.textSmaller,
+                                completed ? { color: Colors.selectionColorLight } : styles.inProgress]}>{detail}</Text>
+      }
+    </View>
+  );
 };
 
 export default AchievementBadge;
