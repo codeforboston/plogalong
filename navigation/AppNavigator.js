8761e9bf064039a23f15e801e599997add41d6cc
Brian Sanders <bdougsand+cfb@gmail.com>
2020-07-01 18:57:24 -0400

    Rewrote top-level navigators
    Unified and simplified how navigation events are triggered in response to state
    changes

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,10 +1,17 @@
 import * as React from 'react';
-import { NavigationContainer, useLinking } from '@react-navigation/native';
+import { YellowBox } from 'react-native';
+import { connect } from 'react-redux';
+import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { Linking } from 'expo';
 
+import { auth } from '../firebase/init';
+import { parseURL } from '../util';
+import { processAchievement } from '../util/users';
+import { useEffectWithPrevious } from '../util/react';
+import actions from '../redux/actions';
+
 import Header from '../components/Header';
-import Loading from '../components/Loading';
 
 import InviteModalScreen  from '../screens/InviteModalScreen';
 import MainTabNavigator from './MainTabNavigator';
@@ -19,68 +26,88 @@ import AchievementModal from '../screens/AchievementModal';
 
 import icons from '../icons';
 
-
+YellowBox.ignoreWarnings(['We found non-serializable values in the navigation state']);
 const AppStack = createStackNavigator();
 
-const urlPrefix = Linking.makeUrl('/');
-const linkingConfig = {
-  ForgotPassword: {
-    path: 'forgot'
-  },
-  Signup: {
-    path: 'signup'
-  },
-  ChangePassword: {
-    path: 'changePassword',
-  },
-  Main: {
-    path: ''
-  },
-};
+const AppNavigator = ({ currentUser, preferences, flashMessage }) => {
+  const ref = React.useRef();
+  const url = Linking.useUrl();
 
+  useEffectWithPrevious(async (lastURL, prevUser) => {
+    if (!ref.current)
+      return;
 
-export default (props) => {
-const ref = React.useRef();
+    const navigation = ref.current;
 
-  const { getInitialState } = useLinking(ref, {
-    prefixes: [urlPrefix],
-    config: linkingConfig
-  });
+    if (url && lastURL !== url) {
+      const parsed = parseURL(url);
 
-  const [isReady, setIsReady] = React.useState(false);
-  const [initialState, setInitialState] = React.useState();
-
-  React.useEffect(() => {
-    getInitialState()
-      .catch(() => {})
-      .then(state => {
-        if (state !== undefined) {
-          setInitialState(state);
+      if (parsed) {
+        if (parsed.params['mode'] === 'verifyEmail') {
+          try {
+            await auth.applyActionCode(parsed.params['oobCode']);
+            flashMessage('Your email address is now confirmed.');
+          } catch (err) {
+            flashMessage(err.toString());
+          }
+          return;
+        } else if (parsed.params['mode'] === 'resetPassword') {
+          navigation.navigate('ChangePassword', parsed.params);
+          return;
         }
+      }
+    }
 
-        setIsReady(true);
-      });
-  }, [getInitialState]);
+    if (prevUser === undefined) {
+      if (!preferences.sawIntro) {
+        navigation.navigate('Intro');
+      } else if (!currentUser) {
+        navigation.navigate('Login');
+      }
+    } else if (prevUser !== currentUser) {
+      if (!currentUser) {
+        navigation.navigate('Login');
+      } else if (prevUser && currentUser && prevUser.uid === currentUser.uid) {
+        const {data} = currentUser;
+        const {data: prevData} = prevUser;
 
-  if (!isReady) {
-    return <Loading />;
-  }
-    return (
-    <NavigationContainer initialState={initialState} ref={ref}>
-      <AppStack.Navigator mode="modal" headerMode="screen">
+        if (prevData && data.achievements && !prevData.notLoaded) {
+          const prevAchievements = prevData.achievements || {};
+          // This code is structured as if we wanted to find ALL the newly
+          // completed achievements. At least for now, though, we're just
+          // showing one modal.
+
+          for (const k of Object.keys(data.achievements)) {
+            if ((!prevAchievements[k] || !prevAchievements[k].completed) &&
+                data.achievements[k].completed) {
+              navigation.navigate('AchievementModal', {
+                achievement: processAchievement(data.achievements, k)
+              });
+              return;
+            }
+          }
+        }
+      }
+    }
+  }, [url, currentUser]);
+
+  return (
+    <NavigationContainer ref={ref}>
+      <AppStack.Navigator mode="modal" headerMode="screen" initialRouteName="Main">
         <AppStack.Screen name="Main"
-                         component={ MainTabNavigator}
                          options={{
-                             headerTitle: (props) => {
-                                 return <Header text={props.children} icon={icons[props.children]} />;
-                             },
-                             headerTitleAlign: 'center',
-                             headerStyle: {
-                                 backgroundColor: '#fff',
-                                 borderBottomColor: 'purple',
-                                 borderBottomWidth: 4,
-                             }
-                         }} />
+                           headerTitle: (props) => {
+                             return <Header text={props.children} icon={icons[props.children]} />;
+                           },
+                           headerTitleAlign: 'center',
+                           headerStyle: {
+                             backgroundColor: '#fff',
+                             borderBottomColor: 'purple',
+                             borderBottomWidth: 4,
+                           }
+                         }}>
+          {props => <MainTabNavigator currentUser={currentUser} {...props} />}
+      </AppStack.Screen>
         <AppStack.Screen name="Intro" component={ ScreenSlider} options={{ headerShown: false }}/>
         <AppStack.Screen name="PhotoViewer" component={ PhotoViewer} options={{ headerShown: false }}/>
         <AppStack.Screen name="Login" component={ LoginScreen } options={{ headerShown: false }}/>
@@ -92,4 +119,12 @@ const ref = React.useRef();
         <AppStack.Screen name="Invite" component={ InviteModalScreen } options={{ headerShown: false }}/>
       </AppStack.Navigator>
     </NavigationContainer>
-)};
+  );
+};
+
+export default connect(state => ({
+  currentUser: state.users.current,
+  preferences: state.preferences,
+}), dispatch => ({
+  flashMessage(...args) { dispatch(actions.flashMessage(...args)); }
+}))(AppNavigator);
--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -1,17 +1,20 @@
 import * as React from 'react';
 import {
-  Linking,
   PixelRatio,
   StyleSheet,
 } from 'react-native';
+import { Linking } from 'expo';
 import { connect } from 'react-redux';
 import { createBottomTabNavigator, BottomTabBar } from '@react-navigation/bottom-tabs';
 
 import { auth } from '../firebase/init';
 import icons from '../icons';
 import Colors from '../constants/Colors';
+import { parseURL } from '../util';
 import { processAchievement } from '../util/users';
-import { flashMessage } from '../redux/actions';
+import { useEffectWithPrevious } from '../util/react';
+
+import Loading from '../components/Loading';
 
 import PlogScreen from '../screens/PlogScreen';
 import HistoryScreen from '../screens/HistoryScreen';
@@ -62,98 +65,30 @@ const TabBarComponent = props => <BottomTabBar {...props}
 
 const Tab = createBottomTabNavigator();
 
-export default connect(state => ({
-    currentUser: state.users.current,
-    preferences: state.preferences,
-}), dispatch => ({
-  flashMessage(...args) { dispatch(flashMessage(...args)); }
-}))(class extends React.Component {
-    componentDidMount() {
-      Linking.addEventListener('url', this.onURLChanged);
+const MainTabNavigator = ({ currentUser, navigation, route, ...props }) => {
+  const childRouteName = route.state ? route.state.routes[route.state.index].name : 'Plog';
+  navigation.setOptions({
+    title: decamel(routeName(route, 'Plog')),
+    headerShown: childRouteName !== 'More',
+  });
 
-        const sawIntro = this.props.preferences.sawIntro;
-        const {navigation} = this.props;
+  if (!currentUser)
+    return <Loading />;
 
-        if (!sawIntro) {
-            navigation.navigate('Intro');
-        } else if (!this.props.currentUser) {
-          navigation.replace('Login');
-        }
-    }
+  const pr = PixelRatio.getFontScale();
 
-  componentWillUnmount() {
-    Linking.removeEventListener('url', this.onURLChanged);
-  }
-
-  onURLChanged = async ({ url }) => {
-    const [_, search] = url.split('?');
-    const params = !search ? [] : search.split('&').reduce((p, kv) => {
-      const [k, v] = kv.split('=');
-      p[decodeURIComponent(k)] = decodeURIComponent(v);
-      return p;
-    }, {});
-
-    if (params['mode'] === 'verifyEmail') {
-      await auth.applyActionCode(params['oobCode']);
-      this.props.flashMessage('Your email address is now confirmed.');
-    }
-  }
-
-    componentDidUpdate({ currentUser: prevUser }) {
-      const {currentUser} = this.props;
-
-      if (prevUser && !currentUser) {
-            this.props.navigation.replace("Login");
-        } else if (currentUser && prevUser && currentUser.uid === prevUser.uid) {
-          const {data} = currentUser;
-          const {data: prevData} = prevUser;
-
-          if (prevData && data.achievements && !prevData.notLoaded) {
-            const prevAchievements = prevData.achievements || {};
-            // This code is structured as if we wanted to find ALL the newly
-            // completed achievements. At least for now, though, we're just
-            // showing one modal.
-
-            // const newAchievements = [];
-            for (const k of Object.keys(data.achievements)) {
-              if ((!prevAchievements[k] || !prevAchievements[k].completed) &&
-                  data.achievements[k].completed) {
-                    this.props.navigation.navigate('AchievementModal', {
-                      achievement: processAchievement(data.achievements, k)
-                     });
-                    return;
-              }
-            }
-          }
-        }
-    }
-
-    render() {
-        const {currentUser, navigation, route, ...props} = this.props;
-        const pr = PixelRatio.getFontScale();
-
-        const childRouteName = route.state ? route.state.routes[route.state.index].name : 'Plog';
-        navigation.setOptions({
-            title: decamel(routeName(route, 'Plog')),
-            headerShown: childRouteName !== 'More',
-        });
-
-        if (!currentUser)
-            return null;
-
-        return (
-            <Tab.Navigator initialRouteName="Plog"
-                           tabBar={TabBarComponent}>
-              {Tabs.map(([name, component]) =>
-                        <Tab.Screen name={name}
-                                    key={name}
-                                    component={component}
-                                    options={makeTabOptions(name, childRouteName, pr > 1.5)}/>
-                       )}
-            </Tab.Navigator>
-        );
-    }
-});
+  return (
+    <Tab.Navigator initialRouteName="Plog"
+                   tabBar={TabBarComponent}>
+      {Tabs.map(([name, component]) =>
+                <Tab.Screen name={name}
+                            key={name}
+                            component={component}
+                            options={makeTabOptions(name, childRouteName, pr > 1.5)}/>
+               )}
+    </Tab.Navigator>
+  );
+};
 
 const styles = StyleSheet.create({
   tabIcon: {
@@ -161,3 +96,4 @@ const styles = StyleSheet.create({
     aspectRatio: 1
   }
 });
+export default MainTabNavigator;
