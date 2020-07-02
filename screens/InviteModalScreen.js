5be0b8fa3f37a1b621ee72b5cc206ce064902d39
Valerie Kenyon <35377991+ValerieKenyon@users.noreply.github.com>
2020-07-02 16:16:57 -0400

    temporary work for issue 15 Invite (share to social media)

--- screens/InviteModalScreen.js
+++ screens/InviteModalScreen.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { useState, useCallback } from 'react';
 import {
+  ActionSheetIOS,
   SafeAreaView,
   StyleSheet,
   Text,
@@ -8,7 +9,13 @@ import {
   Modal,
   Clipboard,
   TextInput,
+  Share,
 } from 'react-native';
+import { 
+  ActionSheetProvider,
+  connectActionSheet,
+  useActionSheet
+} from '@expo/react-native-action-sheet';
 // import Clipboard from "@react-native-community/clipboard";
 import Button from '../components/Button';
 import Colors from '../constants/Colors';
@@ -16,88 +23,6 @@ import DismissButton from '../components/DismissButton';
 import { ShareDialog } from 'react-native-fbsdk';
 import $S from '../styles';
 
-/*
-class InviteModalScreen extends React.Component {
-  constructor(props){
-    super(props);
-    this.state={
-      shareLinkContent : {
-        contentType: 'link',
-        contentUrl: "http://www.plogalong.com",
-        contentDescription: "Jus lil ol me, ploggin along",
-      },
-    }
-  }
-
-  /*
-  writeToClipboard = useCallback(async () => {
-    await Clipboard.setString("http://www.plogalong.com");
-  }, []);
-
-  shareTo = useCallback(() => {}, []);
-  */
-  /*
-  shareLinkWithShareDialog() {
-    var tmp = this;
-    ShareDialog.canShow(tmp.state.shareLinkContent)
-      .then(function(canShow) {
-        if (canShow) {
-          return ShareDialog.show(tmp.state.shareLinkContent);
-        }
-      })
-      .then(function(result) {
-        console.log('handle result: ' + result);
-      },
-      function(error) {
-        console.log('Share fail with error: ' + error);
-      });
-  };
-
-  render() {
-    return (
-      <Modal
-        visible={isInviteModalVisible}
-        animationType="slide"
-        transparent={false}
-        onRequestClose={toggleIsInviteModalVisible}
-      >
-        <SafeAreaView style={styles.modal}>
-          <View style={styles.modalControls}>
-            <DismissButton
-              color="white"
-              title="back"
-              onPress={toggleIsInviteModalVisible}
-            />
-          </View>
-          <View style={styles.inviteModalContainers}>
-            <Text style={[$S.h1, { textAlign: 'center' }]}>Invite</Text>
-            <TextInput
-              value={PLOGALONG_LINK}
-              selectTextOnFocus
-              style={[styles.textInput, styles.inviteContainer]}
-            />
-            <Button title="Copy Link" onPress={writeToClipboard} style={styles.copyButton} />
-          </View>
-          <View style={styles.inviteModalContainers}>
-            <Button title="Share on Facebook" onPress={shareLinkWithShareDialog} style={styles.shareButtons} />
-          </View>
-          <View style={styles.inviteModalContainers}>
-            <Button title="Share on Twitter" onPress={shareTo} style={styles.shareButtons} />
-          </View>
-          <View style={styles.inviteModalContainers}>
-            <Button title="Share on Instagram" onPress={shareTo} style={styles.shareButtons} />
-          </View>
-          <View style={{ flex: 1 }}/>
-        </SafeAreaView>
-      </Modal>
-    );
-  }
-}
-
-export default InviteModalScreen;
-
-*/
-/** */
 export default InviteModalScreen = ({isInviteModalVisible, toggleIsInviteModalVisible}) => {
   // https://natehoffelder.com/blog/making-your-own-custom-facebook-and-twitter-share-links-buttons-for-beginners/
   // 
@@ -112,26 +37,60 @@ export default InviteModalScreen = ({isInviteModalVisible, toggleIsInviteModalVi
     await Clipboard.setString(PLOGALONG_LINK);
   }, [PLOGALONG_LINK]);
 
-  const shareTo = useCallback(() => {}, []);
-
-  const shareLinkWithShareDialog = useCallback(() => {
-//    let tmp = this;
-    ShareDialog.canShow(SHARE_LINK_CONTENT).then(canShow => {
-      if (canShow) {
-        return ShareDialog.show(SHARE_LINK_CONTENT);
-      }
-    }).then(result => {
-      if (result.isCancelled) {
+  /*
+  const { showActionSheetWithOptions } = useActionSheet();
+  
+  const shareTo = useCallback(() => {
+    ActionSheetIOS.showActionSheetWithOptions({
+      //url: PLOGALONG_LINK,
+      options: [
+        "Cancel",
+        "Share on Facebook",
+        "Share on Twitter",
+        "Share on Instagram",
+      ],
+      cancelButtonIndex: 0,
+      message: "Jus lil ol me, ploggin along - " + PLOGALONG_LINK,
+      title: "Share on your favorite app",
+      //dialogTitle: "Share on your favorite app", // dialogTitle is from the expo docs, as an option for Android. The Android docs describe this under the DialogPreference class, which has been deprecated. So dialogTitle may be unnecessary or it may not do anything. The documentation I found is: https://developer.android.com/reference/android/preference/DialogPreference#attr_android:dialogTitle
+    }, 
+    /* console.error, result => {
+      console.log(result);
+    } */
+    /* buttonIndex => {
+      if (buttonIndex === 0) {
         alert('Share operation was cancelled');
       } else {
-        alert('Share was successful with postId: '
-          + result.postId);
+        alert('Share operation was successful');
       }
-    }, error => {
-        alert('Share failed with error: ' + error.message);
-    });
+    }
+    );
   }, []);
+  */
 
+  // https://docs.expo.io/versions/latest/react-native/share/
+  const shareTo = async () => {
+    try {
+      const result = await Share.share({
+        url: PLOGALONG_LINK,
+        message: SHARE_LINK_CONTENT.contentDescription,
+        title: "Share our link on social media",
+      });
+      if (result.action === Share.sharedAction) {
+        if (result.activityType) {
+          // shared with activity type of result.activityType
+        } else {
+          // shared
+        }
+      } else if (result.action === Share.dismissedAction) {
+        // dismissed
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  ShareDialog.canShow = async () => (true);
   return (
     <Modal
       visible={isInviteModalVisible}
@@ -157,20 +116,20 @@ export default InviteModalScreen = ({isInviteModalVisible, toggleIsInviteModalVi
           <Button title="Copy Link" onPress={writeToClipboard} style={styles.copyButton} />
         </View>
         <View style={styles.inviteModalContainers}>
-          <Button title="Share on Facebook" onPress={shareLinkWithShareDialog} style={styles.shareButtons} />
+          <Button title="Share on your favorite app" onPress={shareTo} style={styles.shareButtons} />
+{/*          <Button title="Share on Facebook" onPress={shareLinkWithShareDialog} style={styles.shareButtons} />
         </View>
         <View style={styles.inviteModalContainers}>
           <Button title="Share on Twitter" onPress={shareTo} style={styles.shareButtons} />
         </View>
         <View style={styles.inviteModalContainers}>
-          <Button title="Share on Instagram" onPress={shareTo} style={styles.shareButtons} />
+          <Button title="Share on Instagram" onPress={shareTo} style={styles.shareButtons} /> */}
         </View>
         <View style={{ flex: 1 }}/>
       </SafeAreaView>
     </Modal>
   );
 }
-/* */
 
 const styles = StyleSheet.create({
   modal: {
