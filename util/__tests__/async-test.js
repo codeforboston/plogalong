1e098f2c1b381ba501a1479b129b996d45fd5982
Brian Sanders <bdougsand+cfb@gmail.com>
2020-07-10 18:38:47 -0400

    Added test for rateLimit util function

--- util/__tests__/async-test.js
+++ util/__tests__/async-test.js
@@ -31,3 +31,26 @@ describe('Queue', () => {
     expect(Object.values(results).reduce((n, l) => n+l.length, 0)).toBe(100);
   });
 });
+
+describe('Rate limit', () => {
+  it('', async () => {
+    const results = [];
+    const limitedFn = rateLimited(val => {
+      results.push(val);
+    }, 50);
+
+    await new Promise(resolve => {
+      let i = 0;
+      const intv = setInterval(() => {
+        limitedFn(Date.now());
+
+        if (i++ >= 20) {
+          clearInterval(intv);
+          resolve();
+        }
+      }, 10);
+    });
+
+    expect(results).toHaveLength(5);
+  });
+});
