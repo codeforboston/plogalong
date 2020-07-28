8fa8f88b9df9611d32806592ab6de4cb35c2bdd4
Brian Sanders <bdougsand+cfb@gmail.com>
2020-07-28 17:40:01 -0400

    Android deep linking config

--- firebase/project/web/config.js
+++ firebase/project/web/config.js
@@ -1,5 +1,12 @@
 module.exports = {
   ios: {
     appID: "JQXQCN3XB5.com.plogalong.plogalong"
+  },
+  android: {
+    namespace: 'Plogalong',
+    package: 'com.plogalong',
+    certFingerprints: JSON.stringify([
+
+    ])
   }
 };
