abf88a93a6497d24549a73cfbf67845299b6fb0a
Brian Sanders <bdougsand+cfb@gmail.com>
2020-09-17 12:24:57 -0400

    Added IPA download manifest to simplify installation for testers

--- firebase/project/web/config.js
+++ firebase/project/web/config.js
@@ -1,6 +1,14 @@
+const cp = require('child_process');
+
+let ipaURL;
+try {
+  ipaURL = cp.execFileSync('expo', ['url:ipa', '../../..']).toString().trim();
+} catch(_) {}
+
 module.exports = {
   ios: {
-    appID: "JQXQCN3XB5.com.plogalong.plogalong"
+    appID: "JQXQCN3XB5.com.plogalong.plogalong",
+    ipaURL
   },
   android: {
     namespace: 'plogalong',
@@ -8,5 +16,6 @@ module.exports = {
     certFingerprints: [
       'f4:16:dd:f4:24:30:b8:f5:91:4c:34:44:c4:33:01:38:89:7d:9e:ec:f7:65:c4:10:1c:a2:47:f1:f8:66:9c:03'
     ]
-  }
+  },
+  domain: 'app.plogalong.com',
 };
