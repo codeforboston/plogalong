a14c538104a91dfd228bdcac280a1fb95acdc6e7
Brian Sanders <bdougsand+cfb@gmail.com>
2020-10-29 21:56:56 -0400

    Login screen

--- screens/Login.js
+++ screens/Login.js
@@ -1,19 +1,16 @@
 import * as React from 'react';
 import { useCallback } from 'react';
 import {
-  Image,
   Keyboard,
   SafeAreaView,
   StyleSheet,
   Text,
   View,
 } from 'react-native';
-import { connect } from 'react-redux';
 import {KeyboardAwareScrollView as ScrollView} from 'react-native-keyboard-aware-scrollview';
 
 import * as actions from '../redux/actions';
-import * as AppleAuthentication from 'expo-apple-authentication';
-import { useCurrentUser, useSelector, useDispatch } from '../redux/hooks';
+import { useSelector, useDispatch } from '../redux/hooks';
 import { useParams, useEffectWithPrevious } from '../util/react';
 import { useAppleSignInAvailable } from '../util/native';
 
@@ -27,13 +24,11 @@ import { NavLink } from '../components/Link';
 import Loading from '../components/Loading';
 import PasswordInput from '../components/PasswordInput';
 import TextInput from '../components/TextInput';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 
 
 const LoginForm = (
   {currentUser, disabled, error, params, setter, loginWithGoogle, loginWithApple, onSubmit, loginAnonymously}) => {
-  const appleAuthAvailable = useAppleSignInAvailable();
-
   return (
     <>
       {currentUser &&
@@ -45,7 +40,6 @@ const LoginForm = (
                    autoCapitalize="none"
                    autoCompleteType="email"
                    textContentType="username"
-                   /* ref={input => { this._focusInput = input; }} */
                    keyboardType="email-address"
                    value={params.email}
                    onChangeText={setter('email')}
@@ -65,13 +59,14 @@ const LoginForm = (
               style={[{ marginTop: 20 }]}
               disabled={disabled} />
 
-      <Button title="Login with Google"
-              primary
-              onPress={loginWithGoogle}
-              style={[{ marginTop: 20 }]} />
+      {loginWithGoogle &&
+        <Button title="Login with Google"
+        primary
+        onPress={loginWithGoogle}
+        style={[{ marginTop: 20 }]} />}
 
-      {appleAuthAvailable &&
-       <Button primary
+      {loginWithApple &&
+        <Button primary
                onPress={loginWithApple}
                title="Login with Apple"
        />}
@@ -80,10 +75,10 @@ const LoginForm = (
         Forgot Your Password?
       </NavLink>
 
-      <Link onPress={loginAnonymously}
-            style={styles.centered}>
+      {loginAnonymously &&
+       <Link onPress={loginAnonymously} style={styles.centered}>
         Skip Registration
-      </Link>
+       </Link>}
 
       <NavLink route="Intro" style={[$S.helpLink, styles.centered]}>
         What is Plogging?
@@ -92,67 +87,77 @@ const LoginForm = (
   );
 };
 
-export default () => {
-  const {currentUser, error, loggingIn} = useSelector(({users}) => ({
-    currentUser: users.current,
-    error: users.loginError,
-    loggingIn: users.authenticating,
-  }));
-  const dispatch = useDispatch();
-
-  const {params, setter} = useParams({
-    email: '',
-    password: ''
-  });
-
-  const disabled = !params || !params.email || !params.password;
-  const onSubmit = useCallback(() => {
-    if (disabled) return;
-
-    dispatch(actions.loginWithEmail(params.email, params.password));
-  }, [disabled, params]);
-  const onGoogle = useCallback(() => dispatch(actions.loginWithGoogle()), []);
-  const onApple = useCallback(() => dispatch(actions.loginWithApple()), []);
-  const onSkip = useCallback(() => dispatch(actions.loginAnonymously()), []);
-
-  const navigation = useNavigation();
-
-  React.useEffect(() => {
-    const onBlur = () => {
-      Keyboard.dismiss();
-      dispatch(actions.loginError(null));
-    };
-
-    navigation.addListener('blur', onBlur);
-    return () => { navigation.removeListener('blur', onBlur); };
-  }, []);
-
-  useEffectWithPrevious((wasLoggingIn) => {
-    if (wasLoggingIn && !loggingIn && !error) {
-      navigation.navigate('Main');
-    }
-  }, [loggingIn, error]);
-
+export const LoginScreen = ({ loggingIn, ...props }) => {
   return (
     <SafeAreaView style={$S.safeContainer}>
-      <ScrollView style={[$S.form, loggingIn && styles.loggingIn]}
-                  contentContainerStyle={[$S.container,
-                                          loggingIn && styles.loggingInContainer]}>
-        {loggingIn ?
-         <Loading style={{ marginVertical: 100 }}/> :
-         <LoginForm onSubmit={onSubmit}
-                    disabled={disabled}
-                    params={params}
-                    setter={setter}
-                    error={error}
-                    loginWithGoogle={onGoogle}
-                    loginWithApple={onApple}
-                    loginAnonymously={onSkip}
-         />
-         }
+      <ScrollView style={$S.screenContainer}
+                  contentContainerStyle={[$S.container, $S.scrollContentContainer, loggingIn && styles.loggingInContainer]}>
+      {loggingIn
+       ? <Loading style={{ marginVertical: 100 }}/>
+       : <LoginForm {...props} />
+      }
       </ScrollView>
-    </SafeAreaView>
+      </SafeAreaView>
   );
+};
+
+export default () => {
+    const {currentUser, error, loggingIn} = useSelector(({users}) => ({
+        currentUser: users.current,
+        error: users.loginError,
+        loggingIn: users.authenticating,
+    }));
+
+    const dispatch = useDispatch();
+    const { params: routeParams } = useRoute();
+
+    const {params, setter} = useParams(Object.assign({
+        email: '',
+        password: ''
+    }, routeParams && routeParams.form));
+
+    const disabled = !params || !params.email || !params.password;
+    const onSubmit = useCallback(() => {
+        if (disabled) return;
+
+        dispatch(actions.loginWithEmail(params.email, params.password));
+    }, [disabled, params]);
+    const onGoogle = useCallback(() => dispatch(actions.loginWithGoogle()), []);
+    const onApple = useCallback(() => dispatch(actions.loginWithApple()), []);
+    const onSkip = useCallback(() => dispatch(actions.loginAnonymously()), []);
+
+    const navigation = useNavigation();
+    const appleAuthAvailable = useAppleSignInAvailable();
+
+    React.useEffect(() => {
+        const onBlur = () => {
+            Keyboard.dismiss();
+            dispatch(actions.loginError(null));
+        };
+
+        navigation.addListener('blur', onBlur);
+        return () => { navigation.removeListener('blur', onBlur); };
+    }, []);
+
+    useEffectWithPrevious((wasLoggingIn) => {
+        if (wasLoggingIn && !loggingIn && !error) {
+            navigation.navigate('Main');
+        }
+    }, [loggingIn, error]);
+
+    return (
+        <LoginScreen loggingIn={loggingIn}
+          onSubmit={onSubmit}
+        disabled={disabled}
+        params={params}
+        setter={setter}
+        error={error}
+        loginWithGoogle={onGoogle}
+        loginWithApple={appleAuthAvailable && onApple}
+        loginAnonymously={onSkip}
+        currentUser={currentUser}
+        />
+    );
 };
 
 const styles = StyleSheet.create({
