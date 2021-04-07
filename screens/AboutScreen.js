2dfa281648cc660219195135e54778dea55101ed
Brian Sanders <bdougsand+cfb@gmail.com>
2021-04-06 20:09:33 -0400

    Show version and build number on about screen
    Co-authored-by: Zach J <zachjohnston@users.noreply.github.com>
    Co-authored-by: VA Kenyon <ValerieKenyon@users.noreply.github.com>

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,11 +1,14 @@
 import React from "react";
 import {
+  Platform,
   ScrollView,
   Text,
   View,
 } from "react-native";
+import Constants from 'expo-constants';
 
 import { A, OpenURLButton } from '../components/Link';
+import Colors from "../constants/Colors";
 import $S from '../styles';
 
 const websiteURL = "https://www.plogalong.com/";
@@ -18,7 +21,7 @@ const createdBy =
 export default class AboutScreen extends React.Component {
   render() {
     return (
-      <ScrollView style={$S.container} contentContainerStyle={{ paddingBottom: 30 }}>
+      <ScrollView style={$S.container} contentContainerStyle={{ paddingBottom: 30, flexGrow: 1 }}>
         <View style={$S.bodyContainer}>
           <Text style={$S.body}>
             When you plog, you pick up trash as you go about your daily life... jogging, hiking, or simply walking down the street. Ploggers help keep our neighborhoods, parks, and oceans clean.
@@ -34,6 +37,10 @@ export default class AboutScreen extends React.Component {
 
         <OpenURLButton url={websiteURL}>Visit plogalong.com</OpenURLButton>
         <OpenURLButton url="https://www.facebook.com/Plogalong-100585405000063/">Like us on Facebook</OpenURLButton>
+        <View style={{ flexGrow: 1 }}/>
+        <Text style={{ alignSelf: 'flex-end', color: Colors.activeGray }}>
+          Version {Constants.manifest.version}, build {Platform.OS === 'ios' ? Constants.platform.ios.buildNumber : Platform.OS === 'android' ? Constants.platform.android.versionCode : ''}
+        </Text>
       </ScrollView>
     );
   }
